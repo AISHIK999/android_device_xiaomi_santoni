88c366252f8d0cb8755c708f03fd015c1cad3842
Aishik Mukherjee <aishikm2002@gmail.com>
2022-02-07 20:43:47 +0530

    Import conquer makefiles
    Also cleanup unused makefiles

--- conquer_santoni.mk
+++ conquer_santoni.mk
@@ -1,6 +1,6 @@
 # Inherit makefiles
 $(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
-$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+$(call inherit-product, vendor/conquer/config/common.mk)
 
 # ROM spesific makefile name
-PRODUCT_NAME := havoc_santoni
+PRODUCT_NAME := conquer_santoni
